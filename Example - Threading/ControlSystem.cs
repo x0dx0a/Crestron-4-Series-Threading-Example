6fcd9316e82d2c8e5e9423ca2795a1d73f2e0e66
Randy <randyreyesny@gmail.com>
2021-06-22 20:52:37 -0400

    First commit

---
+++ Example - Threading/ControlSystem.cs
@@ -0,0 +1,128 @@
+using System;
+using Crestron.SimplSharp;                          	// For Basic SIMPL# Classes
+using Crestron.SimplSharpPro;                       	// For Basic SIMPL#Pro classes
+using Crestron.SimplSharpPro.CrestronThread;        	// For Threading
+using Crestron.SimplSharpPro.Diagnostics;		    	// For System Monitor Access
+using Crestron.SimplSharpPro.DeviceSupport;         	// For Generic Device Support
+
+namespace Example_Crestron_Threading
+{
+    
+    public class ControlSystem : CrestronControlSystem
+    {
+        ThreadClass newThreadClass;
+
+        /// <summary>
+        /// ControlSystem Constructor. Starting point for the SIMPL#Pro program.
+        /// Use the constructor to:
+        /// * Initialize the maximum number of threads (max = 400)
+        /// * Register devices
+        /// * Register event handlers
+        /// * Add Console Commands
+        /// 
+        /// Please be aware that the constructor needs to exit quickly; if it doesn't
+        /// exit in time, the SIMPL#Pro program will exit.
+        /// 
+        /// You cannot send / receive data in the constructor
+        /// </summary>
+        public ControlSystem()
+            : base()
+        {
+            try
+            {
+                Thread.MaxNumberOfUserThreads = 20;
+
+                CrestronConsole.AddNewConsoleCommand(StartThread, "StartThread", "Start example thread", ConsoleAccessLevelEnum.AccessAdministrator);
+                CrestronConsole.AddNewConsoleCommand(StopThread, "StopThread", "Stop example thread", ConsoleAccessLevelEnum.AccessAdministrator);
+
+                //Subscribe to the controller events (System, Program, and Ethernet)
+                CrestronEnvironment.ProgramStatusEventHandler += new ProgramStatusEventHandler(_ControllerProgramEventHandler);
+            }
+            catch (Exception e)
+            {
+                ErrorLog.Error("Error in the constructor: {0}", e.Message);
+            }
+        }
+
+        /// <summary>
+        /// InitializeSystem - this method gets called after the constructor 
+        /// has finished. 
+        /// 
+        /// Use InitializeSystem to:
+        /// * Start threads
+        /// * Configure ports, such as serial and verisports
+        /// * Start and initialize socket connections
+        /// Send initial device configurations
+        /// 
+        /// Please be aware that InitializeSystem needs to exit quickly also; 
+        /// if it doesn't exit in time, the SIMPL#Pro program will exit.
+        /// </summary>
+        public override void InitializeSystem()
+        {
+            try
+            {
+
+            }
+            catch (Exception e)
+            {
+                ErrorLog.Error("Error in InitializeSystem: {0}", e.Message);
+            }
+        }
+
+
+        /// <summary>
+        /// Event Handler for Programmatic events: Stop, Pause, Resume.
+        /// Use this event to clean up when a program is stopping, pausing, and resuming.
+        /// This event only applies to this SIMPL#Pro program, it doesn't receive events
+        /// for other programs stopping
+        /// </summary>
+        /// <param name="programStatusEventType"></param>
+        void _ControllerProgramEventHandler(eProgramStatusEventType programStatusEventType)
+        {
+            switch (programStatusEventType)
+            {
+                case (eProgramStatusEventType.Paused):
+                    //The program has been paused.  Pause all user threads/timers as needed.
+                    break;
+                case (eProgramStatusEventType.Resumed):
+                    //The program has been resumed. Resume all the user threads/timers as needed.
+                    break;
+                case (eProgramStatusEventType.Stopping):
+                    //The program has been stopped.
+                    //Close all threads. 
+                    //Shutdown all Client/Servers in the system.
+                    //General cleanup.
+                    //Unsubscribe to all System Monitor events
+                    StopThreadNow();
+
+                    break;
+            }
+
+        }
+
+        private void StartThread(object o)
+        {
+            if (newThreadClass != null)
+            {
+                CrestronConsole.PrintLine("Thread is already running.");
+                return;
+            }
+
+            newThreadClass = new ThreadClass();
+            newThreadClass.Start();
+        }
+
+        private void StopThread(object o)
+        {
+            StopThreadNow();
+        }
+
+        private void StopThreadNow()
+        {
+            if (newThreadClass == null) return;
+
+            newThreadClass.Done = true;
+            newThreadClass = null;
+        }
+    }
+}
---
+++ Example - Threading/ThreadClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Crestron.SimplSharp;                          	// For Basic SIMPL# Classes
+using Crestron.SimplSharpPro.CrestronThread;        	// For Threading
+
+
+namespace Example_Crestron_Threading
+{
+    public class ThreadClass
+    {
+        Thread runningThread;
+        public bool Done;
+
+        public void Start()
+        {
+            Done = false;
+            runningThread = new Thread(ThreadCode, null);
+            runningThread.Start();
+        }
+
+
+        private object ThreadCode(object o)
+        {
+            CrestronConsole.Print("Thread started. Going to print periods until thread is stopped.");
+
+            while (!Done)
+            {
+                CrestronConsole.Print(".");
+                Thread.Sleep(1000);
+            }
+
+            return null;
+        }
+
+    }
+
+}
